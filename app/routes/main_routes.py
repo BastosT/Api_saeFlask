c1fa3c43eef5d8f8b07dab8d8a60dd28d19a5696
BastosT <bastien.taillez74@icloud.com>
2024-12-16 10:55:24 +0100

    route get data

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,9 +1,17 @@
 # app/routes/main_routes.py
 from flask import Blueprint, jsonify
+from influxdb_client import InfluxDBClient
 # from app.services.influx_service import latest_dat
 
 
 main_bp = Blueprint('main', __name__)
+
+
+# Connexion InfluxDB
+client = InfluxDBClient(url="http://51.83.36.122:8086", token="votre_token", org="votre_organisation")  #token du groupe4 orga soit de l'iut ou du groupe4
+query_api = client.query_api()
+
+
 
 @main_bp.route('/')
 def home():
@@ -17,6 +25,32 @@ def home():
 @main_bp.route('/test')
 def test():
     return jsonify({"message": "Hello World!"})
+
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    query = 'from(bucket:"votre_bucket") |> range(start: -7d)'
+    result = query_api.query(query)
+    data = [{"time": record.get_time(), "value": record.get_value()} for table in result for record in table.records]
+    return jsonify(data)
+    
+@app.route('/latest', methods=['GET'])
+def get_latest():
+    query = 'from(bucket:"votre_bucket") |> range(start: -1h)'
+    result = query_api.query(query)
+    data = [{"time": record.get_time(), "value": record.get_value()} for table in result for record in table.records]
+    return jsonify(data)
+
+
+
+
+
+
+
+
+
+
 
 
 # @app.before_first_request
